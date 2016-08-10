30ac3044e5e3d8cd2b82e8551d6284a5a12089eb
sin <must.sow.before.you.can.reap+0001@gmail.com>
2016-08-10 13:44:39 +0900

    Update main.rb

--- main.rb
+++ main.rb
@@ -6,50 +6,89 @@ get '/' do
   erb :index
 end
 
+def pest(pests)
+  object_name = ''
+  result = ''
+  text = ''
+  current = ''
+  subtitle_flg = false
+
+  pests.lines do |pest|
+    pest = pest.chomp.strip
+
+    # insert title
+    result << '<h5>PEST分析</h5>' if pest == '# PEST'
+    return if result.blank?
+
+    # insert sub title
+    object_name = '政治（Politics）' if pest == '# Politics'
+    object_name = '経済（Economics）' if pest == '# Economics'
+    object_name = '社会（Society）' if pest == '# Society'
+    object_name = '技術（Technology）' if pest == '# Technology'
+    if OBJECTS.include?(pest)
+      result << "<tr><td>#{object_name}</td>"
+      subtitle_flg = true
+    elsif subtitle_flg && pest == '--'
+      current = "<td>#{text}</td>"
+      text = ''
+    elsif subtitle_flg && pest.blank?
+      result << "#{current}<td>#{text}</td></tr>"
+      current = ''
+      text = ''
+      subtitle_flg = false
+    elsif subtitle_flg
+      text << (text.blank? ? pest : "<br>#{pest}")
+    end
+  end
+
+result.blank? ? "" : """
+<div class='col s12'>
+  <table class='responsive-table striped'>
+    <thead><th>項目</th><th>現状</th><th>将来</th></thead>
+    <tbody>#{result}</tbody>
+  </table>
+</div>"""
+end
+
 =begin
-* PEST
+curl -F "section_1_result_id=# PEST
 # Politics
 Politics
+AAA
+--
+hogehoge
+fugafuga
 
 # Economics
 Economics
+hoge]
+--
+fuga
+piyo
 
 # Society
 Society
+--
+no time
+hofehoge
+jfoie
 
 # Technology
 Technology
+tutururu
+piyopiyo
+--
+hogehoge
 
+" http://localhost:4567/pest
 =end
-post '/convert' do
-  result = ''
-  flg = {Politics: false,Economics: false,Society: false,Technology: false}
-  params[:section_1_result_id].lines do |pest|
-    p = pest.chomp
+post '/pest' do
+  param = params[:section_1_result_id]
+  pest(param) unless param.blank?
+end
 
-    # insert title
-    result << p if p == '# PEST'
-    return erb :index if result.blank?
-
-    # insert sub title
-    if p == '# Politics'
-      result << p
-      flg[:Politics] = true
-    elsif p == '# Economics'
-      result << p
-      flg[:Economics] = true
-    elsif p == '# Society'
-      result << p
-      flg[:Society] = true
-    elsif p == '# Technology'
-      result << p
-      flg[:Technology] = true
-    end
-
-    # insert content
-
-  end
-
-  @section_1_result = result
+post '/pest_index' do
+  param = params[:section_1_result_id]
+  @section_1_result = pest(param) unless param.blank?
   erb :index
 end
