a377f2feb32682609cc913bb554207dad5a047ba
sin <must.sow.before.you.can.reap+0001@gmail.com>
2016-08-12 17:56:48 +0900

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,56 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'active_support/all'
-
-OBJECTS = ['# Politics','# Economics','# Society','# Technology']
-
-get '/' do
-  erb :index
-end
-
-def pest(pests)
-  object_name = ''
-  result = ''
-  text = ''
-  current = ''
-  subtitle_flg = false
-
-  pests.lines do |pest|
-    pest = pest.chomp.strip
-
-    # insert title
-    result << '<h5>PEST分析</h5>' if pest == '# PEST'
-    return if result.blank?
-
-    # insert sub title
-    object_name = '政治（Politics）' if pest == '# Politics'
-    object_name = '経済（Economics）' if pest == '# Economics'
-    object_name = '社会（Society）' if pest == '# Society'
-    object_name = '技術（Technology）' if pest == '# Technology'
-    if OBJECTS.include?(pest)
-      result << "<tr><td>#{object_name}</td>"
-      subtitle_flg = true
-    elsif subtitle_flg && pest == '--'
-      current = "<td>#{text}</td>"
-      text = ''
-    elsif subtitle_flg && pest.blank?
-      result << "#{current}<td>#{text}</td></tr>"
-      current = ''
-      text = ''
-      subtitle_flg = false
-    elsif subtitle_flg
-      text << (text.blank? ? pest : "<br>#{pest}")
-    end
-  end
-
-result.blank? ? "" : """
-<div class='col s12'>
-  <table class='responsive-table striped'>
-    <thead><th>項目</th><th>現状</th><th>将来</th></thead>
-    <tbody>#{result}</tbody>
-  </table>
-</div>"""
-end
+require './extension/pest.rb'
 
 # API
 
